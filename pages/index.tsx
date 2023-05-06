0f4090ab7865c835e933012ad7f5d4bec10d7362
malizkl <63977569+malizkl@users.noreply.github.com>
2023-05-07 02:02:53 +0300

    changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,13 +32,7 @@ export default function Gallery({ images }: { images: Image[] }) {
   return (
 
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <p className="text-bold text-2xl mb-4 text-center">Av. Mustafa Zafer</p>
-          <p className="text-xl mb-8 text-center text-gray-500">İstanbul Barosu / Fenerbahçe S.K Kongre Üyesi / E.İcra ve İflas Müdürü
-</p>
-
-
-
-
+          <p className="text-bold text-2xl mb-8 text-center">رحلة تركيا 1444🇸🇦🇹🇷</p>
 
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         
