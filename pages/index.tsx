e5642c11d857031774f3ca97172dccba2ee679b0
malizkl <63977569+malizkl@users.noreply.github.com>
2023-05-07 03:22:55 +0300

    changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export async function getStaticProps() {
   const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SUPABASE_URLL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
   )
 
